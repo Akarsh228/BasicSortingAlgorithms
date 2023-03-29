bcdf1518a4b6b71820f2e9db6a85e8d082e650c7
Akarsh Shetty <57933193+Akarsh228@users.noreply.github.com>
2023-03-29 12:48:24 -0700

    fixed file path in merge

--- lovelace_cupcakes/src/main/java/com/sort/MergeSort.java
+++ lovelace_cupcakes/src/main/java/com/sort/MergeSort.java
@@ -8,7 +8,7 @@ public class MergeSort {
 
   public static void main(String[] args) {
     String fileName =
-      "/Users/jerom/Documents/GitHub/class-java/sort/demo/src/main/java/com/sort/cupcake_3906.json";
+      "/Users/akarshshetty/Documents/GitHub/Lovelace_Cupcake/lovelace_cupcakes/src/main/java/com/sort/cupcake_3906.json";
     // String fileName =
     //   "/Users/jerom/Documents/GitHub/class-java/sort/demo/src/main/java/com/sort/cupcake_10.json";
     // String fileName =
